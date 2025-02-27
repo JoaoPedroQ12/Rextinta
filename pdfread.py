c4681000ea8c695b5d736d44b1d8c3d01d4a5ed9
João Pedro <jakelinque789@gmail.com>
2025-02-27 11:49:33 -0400

    Ajustes para retornar os codigos do produto, aprendizado com o re.findall e re.multline.

--- pdfread.py
+++ pdfread.py
@@ -7,6 +7,9 @@ with pdf.open(os.path.join(r'/home/lemon/Downloads', 'Orçamento Nº 0618589.pdf
 
     data = re.search(r'\d{2}/\d{2}/\d{2}', text)
     cliente = re.search(r'C[0-9]+-\s+[A-Za-z\s]+\s', text)
-    print(cliente.group(),'\n' ,data.group())
-    
+    produto = re.findall(r'^\d{6}', text, re.MULTILINE)
+    print(cliente.group(),'\n' ,data.group(), '\n', produto)
+    for a in produto:
+        if a == '020018':
+            print('BRASILAR')
 
