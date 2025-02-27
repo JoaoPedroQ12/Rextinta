5353803c3c59423708fceee3b109eb8b92025cb7
João Pedro <jakelinque789@gmail.com>
2025-02-27 07:35:31 -0400

    Começando a criar o modo de cptura de dados usando regex

--- pdfread.py
+++ pdfread.py
@@ -1,9 +1,12 @@
 import pdfplumber as pdf
 import os 
+import re
 
-with pdf.open(os.path.join(r'/home/lemon/Documents', 'pdf-sample_0.pdf')) as pf:
+with pdf.open(os.path.join(r'/home/lemon/Downloads', 'Orçamento Nº 0618589.pdf')) as pf:
     text = pf.pages[0].extract_text()
 
-    print(text.find('Dummy'))
+    data = re.search(r'\d{2}/\d{2}/\d{2}', text)
+    cliente = re.search(r'C[0-9]+-\s+[A-Za-z\s]+\s', text)
+    print(cliente.group(),'\n' ,data.group())
     
 
