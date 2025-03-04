f35f84ca4fe55eb48e7e244500da8ba14fc7ddac
João Pedro <jakelinque789@gmail.com>
2025-03-03 21:00:52 -0400

    Testado e tudo esta funcionando AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE!!!!!!!!!!!!!!!!, coloquei o test para verificar se as variaveis foram preenchidas e assim avisar, mudei o db para ele aceite valores nulos.

--- rextinta.py
+++ rextinta.py
@@ -4,7 +4,7 @@ import glob
 import re
 import shutil
 import pdfplumber as pdf
-
+# Codes of paint
 tintas = [
     '020036', '020035', '020034', '020033', '020032', '020031',
     '020030', '020029', '020028', '020027', '020026', '020025',
@@ -13,7 +13,7 @@ tintas = [
     '020012'
 ]
 
-
+# Declaration of var
 date = ''
 client = ''
 nmov = ''
@@ -27,6 +27,7 @@ if os.name == 'nt':
 else:
     clear = 'clear'
 
+#Connect with db
 try:
     conn = sqlite3.connect(os.path.join('db_tinta', 'tinta.db'))
     cursor = conn.cursor()
@@ -36,32 +37,43 @@ except Exception as e:
 path_input = os.path.join('raque')
 path_out = os.path.join('deposito')
 
-
 while True:
     try:
-         notes = glob.glob(os.path.join(path_input, "*.pdf"))
+         notes = glob.glob(os.path.join(path_input, "*.pdf")) # search in raque bt files pdfs. 
          if notes:
              os.system(clear)
              for note in notes:
-                 with pdf.open(note) as pf:
+                 with pdf.open(note) as pf: # Search for informations and assignment for var
                      content = pf.pages[0].extract_text()
                      date = re.search(r'\d{2}/\d{2}/\d{2}', content)
                      client = re.search(r'C[0-9]+-\s+[A-Za-z\s]+\s', content)
                      nmov = re.search(r'\d{6}\S', content)
                      product = re.findall(r'^\d{6}', content, re.MULTILINE)
                      color = re.search(r'#[A-Za-z0-9]+\b', content)
-                     for item in product:
+                     if not date: # Verication if the vars they were assingned
+                         print('| Data não encontrada |\n')
+                     elif not client:
+                         print('| Nome do cliente não encontrado | \n')
+                     elif not nmov:
+                         print('| Numero do movimento não encontrado |\n')
+                     elif not product:
+                         print('| Produto não encontrado |\n')
+                     elif not color:
+                         print('| Cor não encontrada |\n')
+                     else:
+                         print(' :) \n')
+                     for item in product: # For paint in note assigenment of db with the information: client, date, nmov(number of moviment), product(paint) and color
                          for code in tintas:
                              if item == code:
                                  product = re.search(fr'{code}\s+([A-Z\s\-\.]+)\s(\d+|,  \d+|\d+)\S(\d+|, \d+|\d+)', content)
                                  print(f"Numero movimento: {nmov.group()}\nCliente: {client.group()}\nData: {date.group()}\nProduto: {product.group()}")
                                  desc = input('Os dados estão correstos(S/N):')
 
-                                 if desc.upper() != 'N':
+                                 if desc.upper() != 'N': # Save in db
                                      qq = input('Quantidade de latas:')
                                      cursor.execute("INSERT INTO rextinta(nmov, nome, data, produto) VALUES (?, ?, ?, ?)", (nmov.group(), client.group(), date.group(), product.group() + ' | Quantidade: ' + qq ))
                                      conn.commit()
-                 shutil.copy2(note, path_out)
+                 shutil.copy2(note, path_out) # moving file pdf for deposito
                  os.remove(note)
          else:
              print('Esperando dados...')
